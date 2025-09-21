88b689cfa190c026fddd42ded53e970a86764c38
George-Njuguna <mailtomegeorge0@gmail.com>
2025-09-21 22:17:20 +0300

    .

--- functions.py
+++ functions.py
@@ -9,19 +9,19 @@ def feat_eng(df):
     df["Time"] = df['Time'].astype(int)
 
     # Rename for consistency
-    #data = data.rename(columns={
-       #'Time': 'time_seconds',
-        #'Class': 'Fraud',
-        #'Amount': 'Ammount'
-    #})
+    df = df.rename(columns={
+       'Time': 'time_seconds',
+        'Class': 'Fraud',
+        'Amount': 'Ammount'
+    })
 
-    #cols = [
-        #"time_seconds", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", 
-        #"V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17", "V18", "V19", "V20",
-        #"V21", "V22", "V23", "V24", "V25", "V26", "V27", "V28", "Ammount", "Fraud", "Timestamp"
-    #]
+    cols = [
+        "time_seconds", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", 
+        "V10", "V11", "V12", "V13", "V14", "V15", "V16", "V17", "V18", "V19", "V20",
+        "V21", "V22", "V23", "V24", "V25", "V26", "V27", "V28", "Ammount", "Fraud", "Timestamp"
+    ]
 
-    return df#[cols]
+    return df[cols]
 
 
 
