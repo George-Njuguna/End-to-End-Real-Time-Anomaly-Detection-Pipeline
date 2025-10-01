3af20656d8c8aab7a40c1067ad5bea97108852a5
George-Njuguna <mailtomegeorge0@gmail.com>
2025-10-01 15:10:30 +0300

    updated model.py

--- Model.py
+++ Model.py
@@ -1,4 +1,21 @@
-import mlflow
 import pandas as pd 
-import sklearn
-import tensorflow
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from functions import import_data
+
+load_dotenv()
+
+ # Connecting to the database 
+engine = create_engine(
+    f"postgresql+psycopg2://{os.getenv('user')}:{os.getenv('password')}@{os.getenv('host')}:{os.getenv('port')}/{os.getenv('database')}"
+    )
+
+train_data = import_data('transactions_train_raw', engine )
+
+
+def main():
+    print("END OF THE MODELLING PIPELINE")
+    
+if __name__ == '__main__':
+    main()
