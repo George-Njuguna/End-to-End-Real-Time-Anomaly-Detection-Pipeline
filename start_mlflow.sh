bf4c636f2b1c722ca400ab4e1062f043988b88dc
George-Njuguna <mailtomegeorge0@gmail.com>
2025-10-05 12:00:21 +0300

    added bash file start_mlflow.sh

---
+++ start_mlflow.sh
@@ -0,0 +1,12 @@
+source .env 
+
+BACKEND_URI="postgresql+psycopg2://${mlflow_user}@${host}:{port}/${mlflow_database}"
+MLFLOW_ARTIFACTS_DIR = "file:${MLFLOW_ARTIFACTS_PATH}" 
+
+echo "Starting MLflow Server..."
+
+exec mlflow server \
+    --backend-store-uri "${BACKEND_URI}" \
+    --artifacts-destination "file:${MLFLOW_ARTIFACTS_DIR}" \
+    --host 0.0.0.0 \
+    --port 5000
