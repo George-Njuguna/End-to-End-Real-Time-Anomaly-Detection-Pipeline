91366769f33464d53147e87f880ca90c8bc25fd7
George-Njuguna <mailtomegeorge0@gmail.com>
2025-10-05 13:59:07 +0300

    Got a running mlflow logger

--- start_mlflow.sh
+++ start_mlflow.sh
@@ -3,12 +3,12 @@
 source .env 
 
 BACKEND_URI="postgresql+psycopg2://${PGUSER}@${PGHOST}:${PGPORT}/${PGDATABASE}"
-MLFLOW_ARTIFACTS_DIR="${MLFLOW_ARTIFACTS_PATH}"
+MLFLOW_ARTIFACTS_DIR="file:${MLFLOW_ARTIFACTS_PATH}"
 
 echo "Starting MLflow Server..."
 
 exec mlflow server \
     --backend-store-uri "${BACKEND_URI}" \
-    --default-artifact-root "file:///${MLFLOW_ARTIFACTS_DIR}" \
+    --default-artifact-root "${MLFLOW_ARTIFACTS_DIR}" \
     --host 0.0.0.0 \
     --port 5000
