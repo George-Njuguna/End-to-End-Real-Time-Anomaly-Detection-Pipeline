5822e72e5cbd6cc2288dd80af0edae1f84b22f7e
George-Njuguna <mailtomegeorge0@gmail.com>
2025-10-07 13:25:38 +0300

    added smote in modelling

--- Model.py
+++ Model.py
@@ -16,7 +16,9 @@ engine = create_engine(
 train_data = import_data('transactions_train_raw', engine )
 
  # Training the model
-precision, recall, f1_score, parameters = modeling_pipe(train_data, True)
+precision, recall, f1_score, parameters = modeling_pipe(train_data, False)
+
+precision1, recall2, f1_score3, parameters4 = modeling_pipe(train_data, True)
 
 
 def main():
