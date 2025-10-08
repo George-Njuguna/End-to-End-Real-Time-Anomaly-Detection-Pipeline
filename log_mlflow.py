9223c917a944bbba73cfd4228b06d32d98916fba
George-Njuguna <mailtomegeorge0@gmail.com>
2025-10-08 22:13:43 +0300

    .

--- log_mlflow.py
+++ log_mlflow.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import seaborn as sns
 
-mlflow.set_tracking_uri( uri = "http:/127.0.0.1:5000" )
+mlflow.set_tracking_uri( uri = "http://127.0.0.1:5000" )
 #mlflow.set_tracking_uri( os.getenv('tracking_uri') )
 mlflow.set_experiment("Fraud_Detection_test")
 
