fedb53594067863a0e302e2ca8f9153e55e33a85
George-Njuguna <mailtomegeorge0@gmail.com>
2025-10-08 14:11:03 +0300

    added mlrun for logistic without smote

---
+++ load_mlflow.py
@@ -0,0 +1,47 @@
+from Model import model1, model2
+import mlflow
+import mlflow.sklearn
+from mlflow.models.signature import infer_signature
+from sklearn.metrics import confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+from datetime import datetime
+import seaborn as sns
+import pandas as pd
+import os 
+
+
+mlflow.set_tracking_uri( os.getenv('tracking_uri') )
+mlflow.set_experiment("Fraud_Detection_test")
+
+timestamp = datetime.now().strftime( "%Y-%m-%d" )
+
+with mlflow.start_run( run_name = f"Logistic_without_smote_{timestamp}" ) as run:
+    mlflow.sklearn.log_model(
+        sk_model = model1[4],
+        artifact_path = "fraud_model",
+        registered_model_name= "fraud_detection"
+    )
+
+    mlflow.log_metric('Precision', model1[0])
+    mlflow.log_metric('Recall', model1[1])
+    mlflow.log_metric('F1 Score', model1[2])
+
+    mlflow.set_tag("dataset_version", "v1")
+    mlflow.set_tag("Imbalance Handling", "None")
+    mlflow.set_tag("Trained_at", f"{timestamp}")
+
+    cm = model1[5]
+    
+    plt.figure(figsize=(6, 5))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted Label")
+    plt.ylabel("True Label")
+    plt.tight_layout()
+    plt.savefig("confusion_matrix.png")
+    plt.close()
+
+    mlflow.log_artifact("confusion_matrix.png")
+
+
+
