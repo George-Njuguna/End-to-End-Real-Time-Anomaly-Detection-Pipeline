5dcca7a29e0d1a0ff6ec75364e8a8b5d5913ad41
George-Njuguna <mailtomegeorge0@gmail.com>
2025-10-21 12:12:13 +0300

    added load_to_postgress.py dag

---
+++ airflow/dags/load_to_postgres.py
@@ -0,0 +1,17 @@
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+from pendulum import timezone
+
+with DAG(
+    dag_id="run_playlist_script_once",
+    start_date=datetime(2024, 1, 1, tzinfo=timezone("Africa/Nairobi")),
+    end_date=datetime(2025, 1, 1, 18, 2, tzinfo=timezone("Africa/Nairobi")),
+    schedule="0 18 * * *",
+    catchup=False
+) as dag:
+
+    run_script = BashOperator(
+        task_id="run_script_1",
+        bash_command="python /opt/airflow/fraud_scripts/script_1.py"
+    )
