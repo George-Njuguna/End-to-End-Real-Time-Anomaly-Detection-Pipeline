2546fca80a65482206c725f89d9b267a0e922c4c
George-Njuguna <mailtomegeorge0@gmail.com>
2025-10-25 16:28:43 +0300

    added condition in load_mlflow.py dag

--- airflow/dags/load_mlflow.py
+++ airflow/dags/load_mlflow.py
@@ -4,13 +4,20 @@ from datetime import datetime
 from pendulum import timezone
 
 with DAG(
-    dag_id="run_script_2_every_week",
+    dag_id="run_script_2_n_3_every_week",
     start_date=datetime(2025, 1, 1, tzinfo=timezone("Africa/Nairobi")),
     schedule="0 10 * * 2",
     catchup=False
 ) as dag:
 
-    run_script = BashOperator(
+    run_script_2 = BashOperator(
         task_id="run_script_2",
         bash_command="python /opt/airflow/fraud_scripts/script_2.py"
     )
+
+    run_script_3 = BashOperator(
+        task_id="run_script_3",
+        bash_command="python /opt/airflow/fraud_scripts/script_3.py"
+    )
+
+    run_script_2 >> run_script_3
