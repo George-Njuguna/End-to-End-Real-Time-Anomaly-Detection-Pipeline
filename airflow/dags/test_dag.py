d59f8b6ab7d2c8cd4523d7d233a11e5b712a2b15
George-Njuguna <mailtomegeorge0@gmail.com>
2025-10-30 13:41:05 +0300

    added test dag

---
+++ airflow/dags/test_dag.py
@@ -0,0 +1,17 @@
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+from pendulum import timezone
+
+with DAG(
+    dag_id="run_test_dag",
+    start_date=datetime(2024, 1, 1, tzinfo=timezone("Africa/Nairobi")),
+    end_date=datetime(2025, 10, 31, 18, 2, tzinfo=timezone("Africa/Nairobi")),
+    schedule="0 18 * * *",
+    catchup=False
+) as dag:
+
+    run_script = BashOperator(
+        task_id="run_test_script",
+        bash_command="python /opt/airflow/fraud_scripts/test.py"
+    )
