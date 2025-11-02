c914de0b646f6afc847f988ea1bf6da570b6de6e
George-Njuguna <mailtomegeorge0@gmail.com>
2025-11-02 12:48:42 +0300

    .

--- airflow/dags/producer.py
+++ airflow/dags/producer.py
@@ -7,7 +7,6 @@ with DAG(
     dag_id="run_producer",
     start_date=datetime(2025, 1, 1, tzinfo=timezone("Africa/Nairobi")),
     schedule="0 10 * * 2",
-    retries=0,
     catchup=False
 ) as dag:
 
