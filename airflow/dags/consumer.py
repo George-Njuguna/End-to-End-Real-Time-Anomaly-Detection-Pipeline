1e0a5d0635d9a449c5fc1609c6e44094a4f52fe1
George-Njuguna <mailtomegeorge0@gmail.com>
2025-11-02 11:51:57 +0300

    split consumer and producer

--- airflow/dags/consumer.py
+++ airflow/dags/consumer.py
@@ -4,20 +4,14 @@ from datetime import datetime
 from pendulum import timezone
 
 with DAG(
-    dag_id="run_producer_consumer",
+    dag_id="run_consumer",
     start_date=datetime(2025, 1, 1, tzinfo=timezone("Africa/Nairobi")),
-    schedule="0 10 * * 2",
+    schedule_interval='@once',
+    retries=0,
     catchup=False
 ) as dag:
 
-    run_script_4_producer = BashOperator(
-        task_id="run_script_4",
-        bash_command="python /opt/airflow/fraud_scripts/script_4_producer.py"
-    )
-
-    run_script_5_consumer = BashOperator(
+   run_script_5_consumer = BashOperator(
         task_id="run_script_5",
         bash_command="python /opt/airflow/fraud_scripts/script_5_consumer.py"
     )
-
-    run_script_4_producer >> [run_script_5_consumer]
---
+++ airflow/dags/producer.py
@@ -0,0 +1,17 @@
+from airflow.models.dag import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from datetime import datetime
+from pendulum import timezone
+
+with DAG(
+    dag_id="run_producer_consumer",
+    start_date=datetime(2025, 1, 1, tzinfo=timezone("Africa/Nairobi")),
+    schedule_interval='@once',
+    retries=0,
+    catchup=False
+) as dag:
+
+    run_script_4_producer = BashOperator(
+        task_id="run_script_4",
+        bash_command="python /opt/airflow/fraud_scripts/script_4_producer.py"
+    )
