af2c738af3063925baeebd0bc2be6f523f81a24f
George-Njuguna <mailtomegeorge0@gmail.com>
2025-11-12 16:14:53 +0300

    .

--- scripts/streamlit_test.py
+++ scripts/streamlit_test.py
@@ -1,27 +1,26 @@
-from pipelines import fetch_batch_data
-import psycopg2
+'''Build an app that:
+1.	Lets user upload a CSV.
+2.	Displays the head() of the DataFrame.
+3.	Has a selectbox for choosing one column.
+4.	Displays summary statistics of that column (df[column].describe()).
+'''
+
+import streamlit as st 
 import pandas as pd
-import os 
 
-import streamlit as st
+st.title('UPLOAD CV HERE')
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
 
-# Connecting to database 
-conn = psycopg2.connect(
-    dbname=os.getenv('POSTGRES_DB'),
-    user=os.getenv('POSTGRES_USER'),
-    password="aninterludecalled",
-    host = "localhost",
-    port="5431"
-)
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    columns = st.multiselect("Choose columns", df.columns.to_list())
+    if columns:
+        st.write('first 10 rows')
+        st.dataframe(df[columns].head(10))
+        st.write('last 10 colums')
+        st.dataframe(df[columns].tail(10))
+        st.dataframe(df[columns].describe(include=['bool','object']))
+        
 
-df = fetch_batch_data('streaming_data' , 3000 , conn )
-
-df = pd.DataFrame(df)
-x = df['fraud']==1
-
-fraud = df[x]
-
-st.write("### Data Table")
-st.dataframe(fraud)  # interactive scrollable table
 
